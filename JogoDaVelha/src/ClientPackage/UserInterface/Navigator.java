fc0e8f8eb13ee8bcbe25efc8ccde443d26065272
dehatanes <debora.buss@hotmail.com>
2018-10-22 00:11:16 -0300

    Alterando Scanner para não ser uma variavel estatica (causa erros)

--- JogoDaVelha/src/ClientPackage/UserInterface/Navigator.java
+++ JogoDaVelha/src/ClientPackage/UserInterface/Navigator.java
@@ -9,24 +9,31 @@ import static ClientPackage.UserInterface.Interfaces.*;
 
 public class Navigator {
 
-    private static Scanner scanner = new Scanner(System.in);
-
     public static String goToStartAndGetNewClient() {
+        Scanner scanner = new Scanner(System.in);
         showStartScreen();
-        return scanner.nextLine();
+        String resp = scanner.nextLine();
+        scanner.close;
+        return resp;
     }
 
     public static String reenterUsername() {
+        Scanner scanner = new Scanner(System.in);
         showUsernameError();
-        return scanner.nextLine();
+        String resp = scanner.nextLine();
+        scanner.close;
+        return resp;
     }
 
     public static void goToLoginError() {
+        Scanner scanner = new Scanner(System.in);
         showFailToLoginScreen();
         scanner.next();
+        scanner.close();
     }
 
     public static int goToMenu(String username) {
+        Scanner scanner = new Scanner(System.in);
         showMenuScreen(username);
         int opt = 0;
         while(true) {
@@ -44,6 +51,7 @@ public class Navigator {
                 opt = 0;
             }
         }
+        scanner.close();
         return opt;
     }
 
@@ -52,16 +60,22 @@ public class Navigator {
     }
 
     public static int goToGamesList(String[] gamesList) {
+        Scanner scanner = new Scanner(System.in);
         showActiveGamesListScreenHeader(gamesList);
-        return scanner.nextInt();
+        int resp = scanner.nextInt();
+        scanner.close;
+        return resp;
     }
 
     public static void showSelectGameError() {
+        Scanner scanner = new Scanner(System.in);
         showInvalidGameOptionMessage();
         scanner.next();
+        scanner.close;
     }
 
     public static void displayGameInWatchMode(GameModel game) {
+        Scanner scanner = new Scanner(System.in);
         showGameScreenHeader(game.id, game.player1, game.player2, game.qtdPeopleWatching);
         showPlayerTurnFragment(game.thisTurnPlayer);
         displayBoardFragment(game.board);
@@ -71,22 +85,32 @@ public class Navigator {
             showWinnerPlayerFragment(game.thisTurnPlayer);
         }
         scanner.next();
+        scanner.close;
     }
 
     public static int displayGameInMyTurnMode(GameModel game) {
+        Scanner scanner = new Scanner(System.in);
         showGameScreenHeader(game.id, game.player1, game.player2, game.qtdPeopleWatching);
         showPlayerTurnFragment(game.thisTurnPlayer);
         displayBoardFragment(game.board);
         showYourTurnFragment();
         int opt;
-        do {
+        while(true) {
             try {
                 opt = scanner.nextInt();
+                if(opt >= 1 || opt <= 9 || game.isValidPosition(game.board, opt)) {
+                    break;
+                } else {
+                    showInvalidBoardOptionMessage();
+                    opt = 0;
+                }
             } catch (Exception e) {
                 showInvalidBoardOptionMessage();
+                scanner.nextLine(); // clean buffer
                 opt = 0;
             }
-        } while (opt < 1 || opt > 9 || !game.isValidPosition(game.board, opt));
+        }
+        scanner.close;
         return opt;
     }
 
@@ -95,7 +119,7 @@ public class Navigator {
         showPlayerTurnFragment(game.thisTurnPlayer);
         displayBoardFragment(game.board);
         showWaitForYourTurnFragment(numWaitingDots);
-        return 0; //????
+        return 0;
     }
 
     public static void displayIwin(GameModel game) {
